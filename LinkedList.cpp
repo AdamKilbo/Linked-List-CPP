4f8710e90cf99e52cee392c7ae7dc457ac9bc4a4
Adam Kilbo <amkilbo@gmail.com>
2015-09-07 23:55:41 -0500

    start of program

---
+++ LinkedList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class dataHolder
+{
+private:
+	dataHolder* parent;
+
+dataholder(dataholder previous)
+{
+	parent = previous
+}
+};
+
+int main()
+{
+
+}
