639b6b40f315819ee72719621456b39bb96663d1
Adam Kilbo <amkilbo@gmail.com>
2015-09-08 23:21:49 -0500

    working program

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
 using namespace std;
 
-class dataHolder
+class LinkedList
 {
-private:
-	dataHolder* parent;
+	struct Node
+	{
+		int x;
+		Node *next;
+	};
+public:
+	LinkedList()
+	{
+		head = NULL;
+	}
 
-dataholder(dataholder previous)
-{
-	parent = previous
-}
+	void addValue(int val)
+	{
+		Node *n = new Node();
+		n->x = val;
+		n->next = head;
+
+		head = n;
+	}
+
+	int popValue()
+	{
+		Node *n = head;
+		int ret = n->x;
+
+		head = head->next;
+		delete n;
+		return ret;
+	}
+
+private:
+	Node *head;
 };
 
 int main()
 {
+	LinkedList list;
+	for (double i = 0; i < 1000; i++)
+		list.addValue(10);
+	list.addValue(5);
+	list.addValue(10);
+	list.addValue(20);
 
+	cout << list.popValue() << endl;
+	cout << list.popValue() << endl;
+	cout << list.popValue() << endl;
+	cout << list.popValue() << endl;
+
+	return 0;	
 }
